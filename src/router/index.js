30e1ddb1a8e3b0cec58663f36cad541394c0f7ed
AlvinSanudharma <alvinsanudharma@gmail.com>
2025-10-31 20:33:22 +0800

    Slicing Login Screen

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
+import Auth from "@/layouts/Auth.vue";
 import Main from "@/layouts/Main.vue";
 import Dashboard from "@/views/Dashboard.vue";
+import Login from "@/views/Login.vue";
 import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
@@ -13,6 +15,24 @@ const router = createRouter({
           path: "",
           name: "dashboard",
           component: Dashboard,
+          meta: {
+            requiresAuth: true,
+            permissions: "dashboard-menu",
+          },
+        },
+      ],
+    },
+    {
+      path: "/login",
+      component: Auth,
+      children: [
+        {
+          path: "",
+          name: "login",
+          component: Login,
+          meta: {
+            requiresUnAuth: true,
+          },
         },
       ],
     },
